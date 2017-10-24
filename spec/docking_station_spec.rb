f5bf5e0fed81c185467755823215e16d4fccf49a
thatdania@gmail.com <thatdania@gmail.com>
2017-10-24 16:27:58 +0100

    Refactored RSPEC to one line

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,12 @@
 require 'docking_station'
 
-describe DockingStation do
+RSpec.describe DockingStation do
+   it { is_expected.to respond_to(:release_bike)}
 end
+
+
+#describe 'Greeter' do
+#  it 'greets Rico' do
+#    expect(greet('Rico')).to eq 'Hello, Rico, how are you today?'
+#  end
+#end
