3fb427563f4ec9a3ac65bab252ff4b4554026fa6
thatdania@gmail.com <thatdania@gmail.com>
2017-10-25 15:26:01 +0100

    Modified the test lines adding an attribute to DockingStation.new and added a test to check if an instance of the DockStationclass has an instance variable bike, which has the string Bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do
-   it { is_expected.to respond_to(:release_bike) }
-   it { expect(DockingStation.new.release_bike.class).to eq Bike }
+   it { expect(DockingStation.new("bike")).to respond_to(:release_bike) }
+   it { expect(DockingStation.new("bike").release_bike.class).to eq Bike }
+   it { expect(DockingStation.new("bike").bike).to eq "bike" }
 end
-
-
-#describe 'Greeter' do
-#  it 'greets Rico' do
-#    expect(greet('Rico')).to eq 'Hello, Rico, how are you today?'
-#  end
-#end
