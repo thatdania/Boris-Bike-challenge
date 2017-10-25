ea8f74429618bce572c307fd187bb37bcfd8a083
Marco Vanali <Marco91Vanali@gmail.com>
2017-10-25 17:16:39 +0100

    Added a test to see whether error exist

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,5 @@ describe DockingStation do
    it { expect(DockingStation.new("bike")).to respond_to(:release_bike) }
    it { expect(DockingStation.new("bike").release_bike.class).to eq Bike }
    it { expect(DockingStation.new("bike").bike).to eq "bike" }
+   it { expect { DockingStation.new("nobikes").release_bike }.to raise_error("There are no bikes at the docking station")}
 end
